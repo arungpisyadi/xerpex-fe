05df2ab52f7c2c100dd65a3e51bdb6f5a322772c
Arung Priyo Isyadi <isyadiarung@gmail.com>
2025-11-24 21:57:39 +0700

    fix: disabled convert to invoice button on bookings details.

--- src/views/BookingDetail.vue
+++ src/views/BookingDetail.vue
@@ -81,7 +81,7 @@
           </button>
 
           <!-- Convert to Invoice Button -->
-          <button
+          <!-- <button
             v-if="booking.status !== 'cancelled' && booking.status !== 'completed'"
             @click="showConvertToInvoiceModal = true"
             :disabled="actionLoading"
@@ -91,7 +91,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
             </svg>
             Convert to Invoice
-          </button>
+          </button> -->
 
           <!-- Edit Button -->
           <button
